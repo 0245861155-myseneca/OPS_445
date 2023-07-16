01bf8948f1038f0e05569d1490ad8775f7122c9d
Tyler Kirkwood <tkirkwood@myseneca.ca>
2023-07-16 17:53:18 -0400

    commit -m 'adding files to ops445'

---
+++ Python3_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+'''
+author Tyler Kirkwood
+
+
+'''
+x = int(input('enter number'))
+
+if x > 0:
+    print("this number is positive")
+elif x < 0:
+    print("number is negative")
+else:
+    print("the number you entered is zero")
---
+++ class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# author name: Tyler Kirkwood
+
+class Person:
+  def __init__(self, name, age, food):
+    self.name = name
+    self.age = age
+    self.food = food
+
+p1 = Person("Tyler", 26, "pizza")
+
+print(p1.name)
+print(p1.age)
+print(p1.food)
+print(f'Hi {p1.name} your {p1.age} and your favourite food is {p1.food}')
